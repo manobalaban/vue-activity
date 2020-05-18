52e5d0cf8d7f6e6f609d75fe0026bece3dbdb5cc
Mano Fabian <mano.b.fabian@gmail.com>
2020-05-18 15:08:35 +0200

    big commit

---
+++ src/components/activityItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <activityItemUpdate v-if="isUpdateActive"
+                        :activity="activity"
+                        :categories="categories"
+                        @toggleUpdate="changeUpdateState" />
+    <activityItemDetail v-else
+                        :activity="activity"
+                        :categories="categories"
+                        @toggleUpdate="changeUpdateState" />
+  </div>
+</template>
+
+<script>
+import activityItemDetail from "./activityItemDetail"
+import activityItemUpdate from "./activityItemUpdate"
+export default {
+   components: {
+     activityItemDetail, 
+     activityItemUpdate
+   },
+   props: {
+     activity: {
+       type: Object,
+       required: true
+     },
+     categories: {
+       type: Object,
+       required: true
+     }
+   },
+   data() {
+     return {
+       isUpdateActive: false
+     }
+   },
+   methods: {
+     changeUpdateState(isUpdate) {
+       this.isUpdateActive = isUpdate
+     }
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/theNavbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="navbar is-white">
+    <div class="container">
+      <div class="navbar-menu is-active">
+        <div class="navbar-start">
+          <a
+            class="navbar-item"
+            :class="{'is-active': filterOption === 'all'}"
+            @click="emitFilter('all')"
+          >All</a>
+          <a
+            class="navbar-item"
+            :class="{'is-active': filterOption === 'inprogress'}"
+            @click="emitFilter('inprogress')"
+          >In Progress</a>
+          <a
+            class="navbar-item"
+            :class="{'is-active': filterOption === 'finished'}"
+            @click="emitFilter('finished')"
+          >Finished</a>
+          <a
+            class="navbar-item"
+            :class="{'is-active': filterOption === 'notstarted'}"
+            @click="emitFilter('notstarted')"
+          >Not Started</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filterOption: "all"
+    };
+  },
+  methods: {
+    emitFilter(filterOption) {
+      this.filterOption = filterOption;
+      this.$emit("filterSelected", filterOption);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
